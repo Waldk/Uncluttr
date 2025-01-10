243f637d26233c9f564baedeca0f6becaa46b1e2
Waldeck <Waldeck.ernst.felix@gmail.com>
2025-01-10 23:02:22 +0100

    add creation of .exe

--- uncluttr/core/main.py
+++ uncluttr/core/main.py
@@ -1,4 +1,3 @@
-# import threading, signal, sys
 import multiprocessing, signal, sys
 from uncluttr.daemon.daemon import start_daemon
 from uncluttr.gui.gui import start_gui
@@ -12,22 +11,19 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     signal.signal(signal.SIGINT, signal_handler)
-
-    # daemon_thread = threading.Thread(target=start_daemon, daemon=True)
-    # gui_thread = threading.Thread(target=start_gui, daemon=True)
-
-    # daemon_thread.start()
-    # gui_thread.start()
-
-    # daemon_thread.join()
-    # gui_thread.join()
 
     daemon_process = multiprocessing.Process(target=start_daemon)
     gui_process = multiprocessing.Process(target=start_gui)
 
-    daemon_process.start()
-    gui_process.start()
-
-    daemon_process.join()
-    gui_process.join()
+    try:
+        daemon_process.start()
+        gui_process.start()
+        daemon_process.join()
+        gui_process.join()
+    finally:
+        daemon_process.terminate()
+        gui_process.terminate()
+        daemon_process.join()
+        gui_process.join()
