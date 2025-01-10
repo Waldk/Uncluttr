25dcc1c8da47b34d712b4b8019ef6c46141a3b91
Waldeck <Waldeck.ernst.felix@gmail.com>
2025-01-10 16:25:15 +0100

    Clean main branch

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -4,11 +4,9 @@ from uncluttr.gui.gui import open_file, drop_file
 
 class TestGUI(unittest.TestCase):
     def test_open_file(self):
-        # Ajoutez ici des tests pour la fonction open_file
         pass
 
     def test_drop_file(self):
-        # Ajoutez ici des tests pour la fonction drop_file
         pass
 
 if __name__ == "__main__":
