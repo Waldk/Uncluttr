8524127c243c8df5b73b7856247b0b83f29e48d8
Waldeck <Waldeck.ernst.felix@gmail.com>
2025-01-22 03:45:08 +0100

    Display time to initialize models

--- uncluttr/file_treatement/text_preprocessing.py
+++ uncluttr/file_treatement/text_preprocessing.py
@@ -1,6 +1,8 @@
 """ This module contains functions for text preprocessing. """
 
 import re
+import sys
+import time
 import unicodedata
 import spacy
 import nltk
@@ -9,12 +11,30 @@ from nltk.corpus import stopwords
 
 # Télécharger les stopwords de NLTK
 try:
+    start_time = time.time()
     stopwords.words('french')
+    print(f"Les stopwords ont été verifies en {time.time() - start_time:.5f} secondes.")
+    sys.stdout.flush()
 except LookupError:
+    start_time = time.time()
     nltk.download('stopwords')
+    print(f"Les stopwords ont été téléchargés en {time.time() - start_time:.5f} secondes.")
+    sys.stdout.flush()
 
-# Charger le modèle spaCy pour le français
-nlp = spacy.load("fr_core_news_sm")
+# Variable globale pour le modèle spaCy
+nlp = None
+
+def initialize_spacy_model():
+    """Initialiser le modèle spaCy si nécessaire."""
+    global nlp
+    if nlp is None:
+        nlp_start_time = time.time()
+        nlp = spacy.load("fr_core_news_sm")
+        print(f"Modèle spaCy chargé en {time.time() - nlp_start_time:.2f} secondes.")
+    else:
+        print("Le modèle spaCy est déjà chargé.")
+    sys.stdout.flush()
+
 
 # Supprimer les accents pour une optionnalité
 def enlever_accents(text: str) -> str:
@@ -24,6 +44,8 @@ def enlever_accents(text: str) -> str:
 # Prétraitement du texte
 def preprocess_text(texte: str) -> str:
     """Preprocess a text by removing non-alphabetic characters and stopwords, and lemmatizing it."""
+    initialize_spacy_model()
+
     # Texte en minuscules
     texte = texte.lower()
 
